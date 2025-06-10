30c4398fdc39cbf558d974e851948155edd0e57a
Matheus Diniz <matheusdiasdiniz@hotmail.com>
2025-06-10 18:43:46 -0300

    feat: Implementa tela de login

---
+++ core/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import path
+from core import views  
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', views.home_view, name='home'),
+    path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
+]
--- core/views.py
+++ core/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 
-# Create your views here.
+def login_view(request):
+    if request.method == 'POST':
+        email = request.POST.get('email')
+        senha = request.POST.get('senha')
+
+        # Usando username para autenticar, por padrão
+        user = authenticate(request, username=email, password=senha)
+
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, '🐾 Miau! Parece que o e-mail ou a senha estão incorretos. Confira e tente novamente para ajudar nossos amiguinhos a encontrar um lar!')
+    
+    return render(request, 'core/login.html')
+
+@login_required
+def home_view(request):
+    return render(request, 'core/home.html')
+
+def logout_view(request):
+    logout(request)
+    return redirect('login')
